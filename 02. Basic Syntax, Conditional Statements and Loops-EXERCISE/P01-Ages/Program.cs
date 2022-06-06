6127208f49c1a7288f9cf9e681340e5c718c8567
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:54:05 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops-EXERCISE/P01-Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            if (age <= 2)
+            {
+                output = "baby";
+            }
+            else if (age <= 13 )
+            {
+                output = "child";
+            }
+            else if (age <= 19)
+            {
+                output = "teenager";
+            }
+            else if (age <= 65)
+            {
+                output = "adult";
+            }
+            else if (age >= 66)
+            {
+                output = "elder";
+            }
+            Console.WriteLine($"{output}");
+        }
+    }
+}

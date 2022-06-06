dbf459fac172fd80310271690dda016be9124aed
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 23:38:32 +0300

    Add files via upload

---
+++ 03. Data Types and Variables-LAB/P01-Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._CSharp_Fundamentals_Data_Types_and_Variables_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            decimal kilometers = (decimal)(meters / 1000.0f);
+
+            Console.WriteLine($"{kilometers:f2}");
+
+        }
+    }
+}

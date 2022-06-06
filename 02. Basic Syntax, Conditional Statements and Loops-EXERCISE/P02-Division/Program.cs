6c23f0ea5d9c579a7cd4dd9533d9857fa491cb5d
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:56:18 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops-EXERCISE/P02-Division/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2.Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int devider = -1;
+
+            if (num % 10 == 0)
+            {
+                devider = 10;
+            }
+            else if (num % 7 == 0)
+            {
+                devider = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                devider = 6;
+            }
+            else if (num % 3 == 0)
+            {
+                devider = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                devider = 2;
+            }
+
+            if (devider == -1)
+            {
+                Console.WriteLine($"Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {devider}");
+            }
+        }
+    }
+}

591ab65e5b363680d7737552f824cf1e4c679913
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:18:49 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops-LAB/P11-Multiplication Table 2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int time = int.Parse(Console.ReadLine());
+
+            if (time > 10)
+            {
+                Console.WriteLine($"{number} X {time} = {number * time}");
+            }
+            else
+            {
+                for (int i = time; i <= 10; i++)
+                {
+                    Console.WriteLine($"{number} X {i} = {number * i}");
+                }
+            }
+
+
+           
+        }
+    }
+}

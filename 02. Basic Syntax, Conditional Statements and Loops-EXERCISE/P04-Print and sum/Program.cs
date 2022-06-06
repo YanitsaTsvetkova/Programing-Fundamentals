1268f35798b6e07aa58c6dfcb2a4e7d59744e9e6
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:59:00 +0300

    Add files via upload

---
+++ 02. Basic Syntax, Conditional Statements and Loops-EXERCISE/P04-Print and sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4.Print_a_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int iterator = firstNum; iterator <= secondNum; iterator++)
+            {
+                Console.Write($"{iterator} ");
+                sum += iterator;
+            }
+           
+            Console.WriteLine();
+
+            Console.Write($"Sum: {sum}");
+        }
+    }
+}

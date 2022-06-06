bd83945e35e31c3ba6a2a0aa214aafe1baaffcf1
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:16:18 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops-LAB/P09-Sum of Odd Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                int currentOddNumber = 1 + (i * 2);
+                sum += currentOddNumber;
+                Console.WriteLine(currentOddNumber);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}

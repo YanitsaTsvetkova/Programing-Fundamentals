c66cfb1a1611bd5a403f4a644b074a18cf95c771
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 22:19:44 +0300

    Add files via upload

---
+++ 01. Basic Syntax, Conditional Statements and Loops-LAB/P12-Even Number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num;
+
+            //do
+            //{
+            //    num = int.Parse(Console.ReadLine());
+
+            //    if (num % 2 == 0)
+            //    {
+            //        Console.WriteLine($"The number is: {Math.Abs(num)}");
+            //        break;
+            //    }
+            //    Console.WriteLine("Please write an even number.");
+
+            //} while (num % 2 != 0);
+
+
+        
+            int num = int.Parse(Console.ReadLine());
+
+            while (num % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"The number is: {Math.Abs(num)}");
+
+        }
+    }
+}

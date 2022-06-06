e3dd4a0ac71d77a45391ce1a43f71099c02334e0
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-06 23:40:02 +0300

    Add files via upload

---
+++ 03. Data Types and Variables-LAB/P02-Pounds to Dollars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal PoundsToDollarsExchangeRate = 1.31m;
+
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+            decimal dolars = pounds * PoundsToDollarsExchangeRate;
+
+            Console.WriteLine($"{Math.Round(dolars, 3, MidpointRounding.AwayFromZero):F3}");
+        }
+    }
+}

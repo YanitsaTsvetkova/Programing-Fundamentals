a0675701231a841f46030bbd1f46b0d46feec234
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:24:22 +0300

    Update Program.cs

--- 04. Data Types and Variables-EXERCISE/P09-Spice Must Flow/Program.cs
+++ 04. Data Types and Variables-EXERCISE/P09-Spice Must Flow/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+using System;
 
-namespace _9._Spice_Must_Flow
+namespace P09._Spice_Must_Flow
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int startFlow = int.Parse(Console.ReadLine());
+
+            int countOfDays = 0;
+            int leftFlow = 0;
+            while (startFlow >= 100)
+            {
+
+                countOfDays++;
+                if (countOfDays == 1)
+                {
+                    leftFlow = startFlow - 26;
+                }
+                else
+                {
+                    leftFlow += startFlow - 26;
+                }
+            
+            startFlow -= 10;
+        }
+                if (leftFlow>=26)
+                {
+                    leftFlow -= 26;
+                }
+                else
+                {
+                    leftFlow = 0;
+                }
+                
+            
+            Console.WriteLine(countOfDays);
+            Console.WriteLine(leftFlow);
+            }
         }
     }
-}
+
+   

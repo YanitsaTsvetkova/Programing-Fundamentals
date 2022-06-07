fc9afe2cc1cb7a6b70f567664973a80ad89a9539
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 15:25:18 +0300

    Add files via upload

---
+++ 03. Data Types and Variables-LAB/P06-Reversed Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _6._Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstChar = Console.ReadLine();
+            var secondChar = Console.ReadLine();
+            var thirdChar = Console.ReadLine();
+
+            Console.WriteLine($"{thirdChar} {secondChar} {firstChar}");
+        }
+    }
+}

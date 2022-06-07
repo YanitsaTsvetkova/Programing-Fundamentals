500d50c87e9058a90108f93558b807dbe8174166
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:07:22 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P06-Triples of Latin Letters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _6._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int letterCount = int.Parse(Console.ReadLine()) + 'a';
+
+            //for (char i = 'a' ; i < letterCount; i++)
+            //{
+            //    for (char j = 'a'; j < letterCount; j++)
+            //    {
+            //        for (char k = 'a'; k < letterCount; k++)
+            //        {
+            //            string letterString = "" + i + j + k;    
+            //            Console.WriteLine(letterString);
+            //        }
+            //    }
+            //}
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('a' + j);
+                        char thirdChar = (char)('a' + k);
+
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}

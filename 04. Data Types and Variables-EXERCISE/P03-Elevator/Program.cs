afcf5f86c486d4f935673891ac35872292a1b95c
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:03:56 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P03-Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            int elevatorCourses = (int)Math.Ceiling((double)peopleCount / elevatorCapacity);
+            Console.WriteLine(elevatorCourses);
+        }
+    }
+}
+

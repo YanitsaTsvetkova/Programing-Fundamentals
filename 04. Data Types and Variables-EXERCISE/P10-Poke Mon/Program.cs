b3955a83cfed7dd9057467ff85d4d3b059b65e06
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:23:43 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P10-Poke Mon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

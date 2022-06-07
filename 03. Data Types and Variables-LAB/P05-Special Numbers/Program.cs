dc04f4e6ee340be76c6f7386a9c6b704bcb42036
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 15:04:16 +0300

    Add files via upload

---
+++ 03. Data Types and Variables-LAB/P05-Special Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5._Special_Numbers
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int numToProcess = i;
+                int sum = 0;
+
+                while (numToProcess != 0)
+                {
+                    int currDigit = numToProcess % 10;
+                    numToProcess /=  10;
+                    sum += currDigit;
+                }
+
+                bool isSpecial = sum == 5 || sum == 7 || sum == 11 ;
+                                
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}

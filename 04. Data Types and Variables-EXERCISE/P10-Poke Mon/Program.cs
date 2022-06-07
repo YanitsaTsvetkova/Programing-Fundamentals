def9b2c52e935f95be5d648cadfae6b3e061f33c
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:34:04 +0300

    Update Program.cs

--- 04. Data Types and Variables-EXERCISE/P10-Poke Mon/Program.cs
+++ 04. Data Types and Variables-EXERCISE/P10-Poke Mon/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
 
-namespace _9._Spice_Must_Flow
+namespace 10._Poke_Mon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int power = int.Parse(Console.ReadLine());
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int originalValue = power;
+            int countTargets = 0;
+            while (power >= distanceBetweenTargets)
+            {
+
+                power -= distanceBetweenTargets;
+                countTargets++;
+
+
+                if (power ==(decimal)(originalValue * 0.50) && exhaustionFactor!=0)
+                {
+                  
+                        power /= exhaustionFactor;
+                    
+                }
+            }
+            if (power < distanceBetweenTargets)
+            {
+                Console.WriteLine(power);
+                Console.WriteLine(countTargets);
+            }
         }
     }
 }

315374cba99f3e259e955bac12a4e6c3dc7e0b22
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:03:05 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P02-Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            do
+            {
+                sum += (num % 10);
+                num /= 10;
+                    
+            } while (num > 0);
+
+            Console.WriteLine(sum); 
+        }
+    }
+}

142453f60c22900ec4fb51965395a79747f3f491
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:04:30 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P04-Sum of Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char cerrCh = char.Parse(Console.ReadLine());
+                int currChCode = (int)cerrCh; // izvlichane na chislov kod ot aski tablica, moje i da se propusne, zashtoto programata si go razbira
+
+                sum += currChCode;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}

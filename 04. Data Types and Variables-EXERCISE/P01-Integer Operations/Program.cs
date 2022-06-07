f36043736efc39706f302981a6f133f68c99a829
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-07 16:02:14 +0300

    Add files via upload

---
+++ 04. Data Types and Variables-EXERCISE/P01-Integer Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._CSharp_Fundamentals_Data_Types_and_Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            long productOne = (long)firstNum + secondNum;
+            long productTwo = productOne / thirdNum;
+            long productThree = productTwo * fourthNum;
+
+            Console.WriteLine(productThree);
+        }
+
+
+    }
+}

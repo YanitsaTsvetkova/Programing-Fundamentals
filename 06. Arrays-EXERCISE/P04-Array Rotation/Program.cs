b3b0b8b0e1e0929aea9e0ebdba5e92b42d5bac2e
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-11 15:26:56 +0300

    Add files via upload

---
+++ 06. Arrays-EXERCISE/P04-Array Rotation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_Arrays_Rortation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //работене с два масива едновременно
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int rotationsCount = int.Parse(Console.ReadLine());
+            int rotationsCountReduced = rotationsCount % arr.Length; // za da ne se bawi programata
+
+            for (int rot = 0; rot < rotationsCount; rot++)
+            {
+                int first = arr[0]; // temorary int
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = first;
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+
+        }
+    }
+}

356e5ab284b25d3899bec911fbfe1948344d0ff7
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-11 15:24:15 +0300

    Add files via upload

---
+++ 06. Arrays-EXERCISE/P02-Common Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] arr2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        Console.Write($"{arr2[j]} ");
+                    }
+                }
+                
+            }
+            Console.WriteLine();
+            
+            //foreach (string element in arr2)
+            //{
+            //    for (int i = 0; i < arr1.Length; i++)
+            //    {
+            //        string elementsOne = arr1[i];
+            //        if (arr2 == arr1)
+            //        { 
+            //            Console.WriteLine(arr2 + " ");
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+    }
+}

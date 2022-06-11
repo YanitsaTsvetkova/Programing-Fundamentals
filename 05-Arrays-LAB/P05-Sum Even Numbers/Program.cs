1d1d14379baf9711f173a801dac68b4ec786dfd3
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-11 15:12:41 +0300

    Add files via upload

---
+++ 05-Arrays-LAB/P05-Sum Even Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            // procutame masiv ot konzolata
+            int sum = 0;
+
+            foreach (int item in items) 
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    int currvalue = items[i];
+            //    if (currvalue % 2 == 0 )
+            //    {
+            //        sum += currvalue;
+            //    }
+            //}
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}

d2f6859dc4d2718c2235d2bc5d433fa6eee0199d
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-11 15:47:40 +0300

    Add files via upload

---
+++ 05. Arrays-LAB/P02-Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNumber = int.Parse(Console.ReadLine());
+
+            int[] items = new int[numberOfNumber];
+
+            for (int i = 0; i < numberOfNumber; i++)
+            {
+                items[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{items[i]} ");
+            }
+           // Console.WriteLine(String.Join(" ", items)); alternativa na gorniq for cikul!
+
+            Console.WriteLine();
+        }
+    }
+}

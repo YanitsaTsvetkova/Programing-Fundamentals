869818c6c9f15541bf2640ef43aec279a84f77a5
Yanitsa Tsvetkova <98115318+YanitsaTsvetkova@users.noreply.github.com>
2022-06-20 17:01:49 +0300

    Add files via upload

---
+++ 06. Arrays-EXERCISE/P10-LadyBugs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _10._LadyBiugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] ladyBugField = new int[fieldSize];
+
+            string[] occupiedIndexes = Console.ReadLine().Split(); // okupirani linii
+
+            for (int i = 0; i < occupiedIndexes.Length; i++)
+            {
+                int currentIndex = int.Parse(occupiedIndexes[i]);
+
+                if (currentIndex >= 0 && currentIndex < fieldSize)
+                {
+                    ladyBugField[currentIndex] = 1; // zapulvame poleto, kudeto ima bubolechka
+                }
+            }
+
+            string[] commands = Console.ReadLine().Split();
+
+            while (commands[0] != "end")
+            {
+                int currentIndex = int.Parse(commands[0]); // znam, che sme vzeli 0 , ot edinicata e stringa
+
+                bool isFirst = true; // dali bubolechkata e purva
+
+                while (currentIndex >= 0 && currentIndex < fieldSize && ladyBugField[currentIndex] != 0) //moga da mestq
+                {
+                    if (isFirst)
+                    {
+                        ladyBugField[currentIndex] = 0;
+                        isFirst = false; // vehe ne e purva
+                    }
+
+                    string direction = commands[1];
+                    int flightLenght = int.Parse(commands[2]);
+
+                    if (direction == "left")
+                    {
+                        currentIndex -= flightLenght;
+
+                        if (currentIndex >= 0 && currentIndex < fieldSize)
+                        {
+                            if (ladyBugField[currentIndex] == 0) // dali mqstoto e 0, t.e e prazno
+                            {
+                                ladyBugField[currentIndex] = 1;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        currentIndex += flightLenght;
+
+                        if (currentIndex >= 0 && currentIndex < fieldSize)
+                        {
+                            if (ladyBugField[currentIndex] == 0)
+                            {
+                                ladyBugField[currentIndex] = 1;
+                                break;
+                            }
+
+                        }
+                    }
+                }
+                commands = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", ladyBugField));
+        }
+    }
+}
